7117e2861619661e087b5b306a469a672af6f030
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 12:09:22 -0400

    Your changing file java/src/com/google/template/soy/jbcsrc/restricted/SoyJbcSrcPrintDirective.java

--- java/src/com/google/template/soy/jbcsrc/restricted/SoyJbcSrcPrintDirective.java
+++ java/src/com/google/template/soy/jbcsrc/restricted/SoyJbcSrcPrintDirective.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2017 Google Inc.
  *
@@ -30,98 +31,94 @@ import java.util.List;
 @SuppressWarnings("deprecation")
 public interface SoyJbcSrcPrintDirective extends SoyPrintDirective {
 
-  /**
-   * Applies this directive on the given value.
-   *
-   * <p>Important note when implementing this method: The value may not yet have been coerced to a
-   * string. You may need to explicitly coerce it to a string using the {@link
-   * SoyExpression#coerceToString()}.
-   *
-   * @param value The value to apply the directive on. This value may not yet have been coerced to a
-   *     string.
-   * @param args The directive's arguments, if any (usually none).
-   * @return The resulting value.
-   */
-  SoyExpression applyForJbcSrc(
-      JbcSrcPluginContext context, SoyExpression value, List<SoyExpression> args);
-
-  /**
-   * A print directive that supports streaming.
-   *
-   * <p>Streaming print directives work by wrapping the {@link LoggingAdvisingAppendable} object.
-   * This means that the {@code value} is not passed directly to the directive, instead the {@code
-   * value} is represented by a sequence of {@link LoggingAdvisingAppendable#append(CharSequence)
-   * append} operations on the wrapped appendable object. It is further more expected that all
-   * non-append operations (e.g. {@link
-   * LoggingAdvisingAppendable#enterLoggableElement(com.google.template.soy.data.LogStatement)}) are
-   * simply proxied through to the underlying object.
-   *
-   * <p>NOTE: any streamable print directive must also support a non-streaming option. The compiler
-   * will prefer to use the streaming option but it will not always be possible or necessary (for
-   * example, if the content is a compile time constant then we may avoid streaming it, or if this
-   * print directive is combined with non-streamable print directives then we will not be able to
-   * stream.
-   */
-  interface Streamable extends SoyJbcSrcPrintDirective {
     /**
-     * A simple object that represents an {@link Expression} for a {@link LoggingAdvisingAppendable}
-     * which will apply a given print directive, as well as additional options for the behavior of
-     * the print directive.
-     */
-    @AutoValue
-    abstract class AppendableAndOptions {
-      /**
-       * Creates an appendable that doesn't need to be closed.
-       *
-       * <p>This means the appendable cannot buffer any content, each {@link
-       * LoggingAdvisingAppendable#append} call must be handled immediately.
-       */
-      public static AppendableAndOptions create(Expression expression) {
-
-        return create(expression, /* closeable= */ false);
-      }
-
-      private static AppendableAndOptions create(Expression expression, boolean closeable) {
-        expression.checkAssignableTo(BytecodeUtils.LOGGING_ADVISING_APPENDABLE_TYPE);
-        return new AutoValue_SoyJbcSrcPrintDirective_Streamable_AppendableAndOptions(
-            expression, /* closeable= */ closeable);
-      }
-
-      /**
-       * Creates an appendable that needs to be closed.
-       *
-       * <p>This means the appendable must implement {@link java.io.Closeable} and that the compiler
-       * will ensure that the {@link java.io.Closeable#close} method will be called when no more
-       * interactions will occur. Implementations can take advantage of this to implement print
-       * directives that use a temporary buffer.
-       */
-      public static AppendableAndOptions createCloseable(Expression expression) {
-        expression.checkAssignableTo(BytecodeUtils.LOGGING_ADVISING_APPENDABLE_TYPE);
-        return create(expression, /* closeable= */ true);
-      }
-
-      /**
-       * The {@link LoggingAdvisingAppendable} expression. Generates code to produce another {@link
-       * LoggingAdvisingAppendable} that when written to applies the directive logic.
-       */
-      public abstract Expression appendable();
-
-      /**
-       * Specifies whether the {@link #appendable()} implements {@link java.io.Closeable} and needs
-       * to have it's {@link java.io.Closeable#close()} method called to function correctly.
-       */
-      public abstract boolean closeable();
-    }
-
-    /**
-     * Applies the directive to a {@link LoggingAdvisingAppendable} object.
+     * Applies this directive on the given value.
      *
-     * @param context The rendering context object.
-     * @param delegateAppendable The delegate appendable
-     * @param args The print directive arguments.
-     * @return An {@link AppendableAndOptions} for the directive.
+     * <p>Important note when implementing this method: The value may not yet have been coerced to a
+     * string. You may need to explicitly coerce it to a string using the {@link
+     * SoyExpression#coerceToString()}.
+     *
+     * @param value The value to apply the directive on. This value may not yet have been coerced to a
+     *     string.
+     * @param args The directive's arguments, if any (usually none).
+     * @return The resulting value.
      */
-    AppendableAndOptions applyForJbcSrcStreaming(
-        JbcSrcPluginContext context, Expression delegateAppendable, List<SoyExpression> args);
-  }
+    SoyExpression applyForJbcSrc(JbcSrcPluginContext context, SoyExpression value, List<SoyExpression> args);
+
+    /**
+     * A print directive that supports streaming.
+     *
+     * <p>Streaming print directives work by wrapping the {@link LoggingAdvisingAppendable} object.
+     * This means that the {@code value} is not passed directly to the directive, instead the {@code
+     * value} is represented by a sequence of {@link LoggingAdvisingAppendable#append(CharSequence)
+     * append} operations on the wrapped appendable object. It is further more expected that all
+     * non-append operations (e.g. {@link
+     * LoggingAdvisingAppendable#enterLoggableElement(com.google.template.soy.data.LogStatement)}) are
+     * simply proxied through to the underlying object.
+     *
+     * <p>NOTE: any streamable print directive must also support a non-streaming option. The compiler
+     * will prefer to use the streaming option but it will not always be possible or necessary (for
+     * example, if the content is a compile time constant then we may avoid streaming it, or if this
+     * print directive is combined with non-streamable print directives then we will not be able to
+     * stream.
+     */
+    interface Streamable extends SoyJbcSrcPrintDirective {
+        /**
+         * A simple object that represents an {@link Expression} for a {@link LoggingAdvisingAppendable}
+         * which will apply a given print directive, as well as additional options for the behavior of
+         * the print directive.
+         */
+        @AutoValue
+        abstract class AppendableAndOptions {
+            /**
+             * Creates an appendable that doesn't need to be closed.
+             *
+             * <p>This means the appendable cannot buffer any content, each {@link
+             * LoggingAdvisingAppendable#append} call must be handled immediately.
+             */
+            public static AppendableAndOptions create(Expression expression) {
+                return create(expression, false);
+            }
+
+            private static AppendableAndOptions create(Expression expression, boolean closeable) {
+                expression.checkAssignableTo(BytecodeUtils.LOGGING_ADVISING_APPENDABLE_TYPE);
+                return new AutoValue_SoyJbcSrcPrintDirective_Streamable_AppendableAndOptions(
+                        expression, closeable);
+            }
+
+            /**
+             * Creates an appendable that needs to be closed.
+             *
+             * <p>This means the appendable must implement {@link java.io.Closeable} and that the compiler
+             * will ensure that the {@link java.io.Closeable#close} method will be called when no more
+             * interactions will occur. Implementations can take advantage of this to implement print
+             * directives that use a temporary buffer.
+             */
+            public static AppendableAndOptions createCloseable(Expression expression) {
+                return create(expression, true);
+            }
+
+            /**
+             * The {@link LoggingAdvisingAppendable} expression. Generates code to produce another {@link
+             * LoggingAdvisingAppendable} that when written to applies the directive logic.
+             */
+            public abstract Expression appendable();
+
+            /**
+             * Specifies whether the {@link #appendable()} implements {@link java.io.Closeable} and needs
+             * to have its {@link java.io.Closeable#close()} method called to function correctly.
+             */
+            public abstract boolean closeable();
+        }
+
+        /**
+         * Applies the directive to a {@link LoggingAdvisingAppendable} object.
+         *
+         * @param context The rendering context object.
+         * @param delegateAppendable The delegate appendable
+         * @param args The print directive arguments.
+         * @return An {@link AppendableAndOptions} for the directive.
+         */
+        AppendableAndOptions applyForJbcSrcStreaming(JbcSrcPluginContext context, Expression delegateAppendable, List<SoyExpression> args);
+    }
 }
