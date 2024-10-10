9adbb5602642087b60741241193fe5cf1b342215
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 17:02:15 -0400

    Your changing file java/src/com/google/template/soy/passes/VeLogValidationPass.java

--- java/src/com/google/template/soy/passes/VeLogValidationPass.java
+++ java/src/com/google/template/soy/passes/VeLogValidationPass.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2017 Google Inc.
  *
@@ -65,47 +66,36 @@ import java.util.Objects;
  * </ul>
  */
 final class VeLogValidationPass implements CompilerFileSetPass {
-  private static final SoyErrorKind UNEXPECTED_DATA =
-      SoyErrorKind.of(
-          "Unexpected data argument. The VE is type ''{0}'' which means there cannot be any data. "
-              + "The data is typed ''{1}'' and must match with the VE.");
-  private static final SoyErrorKind WRONG_TYPE =
-      SoyErrorKind.of("Expected an expression of type ''{0}'', instead got ''{1}''.");
-  private static final SoyErrorKind LOGONLY_DISALLOWED_IN_MSG =
-      SoyErrorKind.of(
-          "The logonly attribute may not be set on '''{velog}''' nodes in '''{msg}''' context. "
-              + "Consider moving the logonly content into another template and calling it, or "
-              + "refactoring your '''{msg}''' into multiple distinct messages.");
-  private static final SoyErrorKind REQUIRE_STRICTHTML =
-      SoyErrorKind.of(
-          "The '{'velog ...'}' command can only be used in templates with stricthtml=\"true\".");
+  private static final SoyErrorKind UNEXPECTED_DATA = SoyErrorKind.of(
+      "Unexpected data argument. The VE is type ''{0}'' which means there cannot be any data. "
+          + "The data is typed ''{1}'' and must match with the VE.");
+  private static final SoyErrorKind WRONG_TYPE = SoyErrorKind.of("Expected an expression of type ''{0}'', instead got ''{1}''.");
+  private static final SoyErrorKind LOGONLY_DISALLOWED_IN_MSG = SoyErrorKind.of(
+      "The logonly attribute may not be set on '''{velog}''' nodes in '''{msg}''' context. "
+          + "Consider moving the logonly content into another template and calling it, or "
+          + "refactoring your '''{msg}''' into multiple distinct messages.");
+  private static final SoyErrorKind REQUIRE_STRICTHTML = SoyErrorKind.of(
+      "The '{'velog ...'}' command can only be used in templates with stricthtml=\"true\".");
 
-  private static final SoyErrorKind INVALID_LOGGING_FUNCTION_LOCATION =
-      SoyErrorKind.of(
-          "The logging function ''{0}'' can only be evaluated in a print command that is the "
-              + "only direct child of an html attribute value.{1}",
-          SoyErrorKind.StyleAllowance.NO_PUNCTUATION);
+  private static final SoyErrorKind INVALID_LOGGING_FUNCTION_LOCATION = SoyErrorKind.of(
+      "The logging function ''{0}'' can only be evaluated in a print command that is the "
+          + "only direct child of an html attribute value.{1}",
+      SoyErrorKind.StyleAllowance.NO_PUNCTUATION);
 
-  private static final SoyErrorKind NO_PRINT_DIRECTIVES =
-      SoyErrorKind.of(
-          "The logging function ''{0}'' can only be evaluated in a print command with no print "
-              + "directives.");
+  private static final SoyErrorKind NO_PRINT_DIRECTIVES = SoyErrorKind.of(
+      "The logging function ''{0}'' can only be evaluated in a print command with no print "
+          + "directives.");
 
-  private static final SoyErrorKind UNKNOWN_PROTO =
-      SoyErrorKind.of("Unknown proto type ''{0}'' configured for use with this VE.");
-  private static final SoyErrorKind BAD_DATA_TYPE =
-      SoyErrorKind.of(
-          "Illegal VE metadata type ''{0}'' for this VE. The metadata must be a proto.");
-  private static final SoyErrorKind INVALID_VE =
-      SoyErrorKind.of(
-          "The velog command requires a VE identifier, an expression of the ''ve'' type or an "
-              + "expression of the ''ve_data'' type. Found an expression of type ''{0}''.");
-  private static final SoyErrorKind VE_UNION_WITH_DATA =
-      SoyErrorKind.of(
-          "It is illegal to set the data parameter if the ve type is a union (''{0}'').");
+  private static final SoyErrorKind UNKNOWN_PROTO = SoyErrorKind.of("Unknown proto type ''{0}'' configured for use with this VE.");
+  private static final SoyErrorKind BAD_DATA_TYPE = SoyErrorKind.of(
+      "Illegal VE metadata type ''{0}'' for this VE. The metadata must be a proto.");
+  private static final SoyErrorKind INVALID_VE = SoyErrorKind.of(
+      "The velog command requires a VE identifier, an expression of the ''ve'' type or an "
+          + "expression of the ''ve_data'' type. Found an expression of type ''{0}''.");
+  private static final SoyErrorKind VE_UNION_WITH_DATA = SoyErrorKind.of(
+      "It is illegal to set the data parameter if the ve type is a union (''{0}'').");
 
-  private static final SoyErrorKind LOG_WITHIN_MESSAGE_REQUIRES_ELEMENT =
-      SoyErrorKind.of("'{velog'} within '{msg'} must directly wrap an HTML element.");
+  private static final SoyErrorKind LOG_WITHIN_MESSAGE_REQUIRES_ELEMENT = SoyErrorKind.of("'{velog'} within '{msg'} must directly wrap an HTML element.");
 
   private final ErrorReporter reporter;
   private final SoyTypeRegistry typeRegistry;
@@ -136,13 +126,10 @@ final class VeLogValidationPass implements CompilerFileSetPass {
         reporter.report(node.getVeDataExpression().getSourceLocation(), REQUIRE_STRICTHTML);
       }
     }
-    // We need to validate logging functions.  The rules are
-    // 1. logging functions can only be the direct children of PrintNodes
-    // 2. the print nodes must be direct children of HtmlAttributeValueNodes
-    //
-    // However, because there is no way (currently) to navigate from an ExprNode to the SoyNode
-    // which owns it, we need to do this multi-phase traversal to ensure the correct parenting
-    // hierarchy.
+    validateLoggingFunctions(template);
+  }
+
+  private void validateLoggingFunctions(TemplateNode template) {
     SoyTreeUtils.visitExprNodesWithHolder(
         template,
         FunctionNode.class,
@@ -155,125 +142,88 @@ final class VeLogValidationPass implements CompilerFileSetPass {
 
   private void validateLoggingFunction(ExprHolderNode holderNode, FunctionNode function) {
     if (function.getParent().getKind() != ExprNode.Kind.EXPR_ROOT_NODE) {
-      reporter.report(
-          function.getSourceLocation(),
-          INVALID_LOGGING_FUNCTION_LOCATION,
-          function.getStaticFunctionName(),
-          " It is part of complex expression.");
+      reportInvalidLoggingFunctionLocation(function, "It is part of complex expression.");
       return;
     }
     if (holderNode.getKind() != SoyNode.Kind.PRINT_NODE) {
-      reporter.report(
-          function.getSourceLocation(),
-          INVALID_LOGGING_FUNCTION_LOCATION,
-          function.getStaticFunctionName(),
-          " It isn't in a print node.");
+      reportInvalidLoggingFunctionLocation(function, "It isn't in a print node.");
       return;
     }
+    validatePrintNode(holderNode, function);
+  }
+
+  private void reportInvalidLoggingFunctionLocation(FunctionNode function, String message) {
+    reporter.report(function.getSourceLocation(), INVALID_LOGGING_FUNCTION_LOCATION, function.getStaticFunctionName(), message);
+  }
+
+  private void validatePrintNode(ExprHolderNode holderNode, FunctionNode function) {
     PrintNode printNode = (PrintNode) holderNode;
     if (printNode.numChildren() != 0) {
-      reporter.report(
-          printNode.getChild(0).getSourceLocation(),
-          NO_PRINT_DIRECTIVES,
-          function.getStaticFunctionName());
+      reporter.report(printNode.getChild(0).getSourceLocation(), NO_PRINT_DIRECTIVES, function.getStaticFunctionName());
     }
     if (holderNode.getParent().getKind() != SoyNode.Kind.HTML_ATTRIBUTE_VALUE_NODE) {
-      reporter.report(
-          function.getSourceLocation(),
-          INVALID_LOGGING_FUNCTION_LOCATION,
-          function.getStaticFunctionName(),
-          " It isn't the direct child of an attribute value.");
+      reportInvalidLoggingFunctionLocation(function, "It isn't the direct child of an attribute value.");
       return;
     }
     if (holderNode.getParent().numChildren() > 1) {
-      reporter.report(
-          function.getSourceLocation(),
-          INVALID_LOGGING_FUNCTION_LOCATION,
-          function.getStaticFunctionName(),
-          " It has sibling nodes in the attribute value.");
+      reporter.report(function.getSourceLocation(), INVALID_LOGGING_FUNCTION_LOCATION, function.getStaticFunctionName(), "It has sibling nodes in the attribute value.");
     }
   }
 
   private void validateVelogElementStructure(VeLogNode node) {
-    List<StandaloneNode> children =
-        node.getChildren().stream()
-            .filter(child -> !SoyElementPass.ALLOWED_CHILD_NODES.contains(child.getKind()))
-            .collect(toImmutableList());
-    // TODO(b/133428199): Support {velog} around calls in messages.
-    if (node.getNearestAncestor(MsgFallbackGroupNode.class) == null
-        && children.size() == 1
-        && Iterables.getLast(children) instanceof CallBasicNode) {
-      node.setNeedsSyntheticVelogNode(true);
-      return;
-    }
+    List<StandaloneNode> children = node.getChildren().stream()
+        .filter(child -> !SoyElementPass.ALLOWED_CHILD_NODES.contains(child.getKind()))
+        .collect(toImmutableList());
 
-    // If {velog} is empty, or does not have a single root, we must output a synthetic VE log node
-    // on the client.
-    if (node.numChildren() == 0) {
+    if (shouldOutputSyntheticVelogNode(node, children)) {
       node.setNeedsSyntheticVelogNode(true);
       return;
     }
 
     HtmlOpenTagNode firstTag = node.getOpenTagNode();
-    // If the first child of {velog} is not an open tag, output a synthetic VE log node.
-    if (firstTag == null) {
+    if (firstTag == null || !isValidFirstTag(firstTag)) {
       node.setNeedsSyntheticVelogNode(true);
-      return;
-    }
-
-    if (!firstTag.getTagName().isStatic() && !firstTag.getTagName().isLegacyDynamicTagName()) {
-      node.setNeedsSyntheticVelogNode(true);
-      return;
-    }
-
-    // If the first child is self-closing or is a void tag, output a synthetic VE log node if we see
-    // anything after it. If it is the only thing, we don't need a synthetic VE log node.
-    if (firstTag.isSelfClosing() || firstTag.getTagName().isDefinitelyVoid()) {
-      if (node.numChildren() > 1) {
-        node.setNeedsSyntheticVelogNode(true);
-      }
       return;
     }
 
     HtmlCloseTagNode lastTag = node.getCloseTagNode();
-    // If the last child is not a close tag, output a synthetic VE log node.
-    if (lastTag == null) {
-      node.setNeedsSyntheticVelogNode(true);
-      return;
-    }
-    // This check make sures that there is exactly one top-level element -- the last tag must
-    // close the first tag within {velog} command. Otherwise, we need to output a synthetic VE log
-    // node.
-    if (lastTag.getTaggedPairs().size() != 1
-        || !Objects.equals(lastTag.getTaggedPairs().get(0), firstTag)) {
+    if (lastTag == null || !isSingleTopLevelElement(firstTag, lastTag)) {
       node.setNeedsSyntheticVelogNode(true);
     }
+  }
+
+  private boolean shouldOutputSyntheticVelogNode(VeLogNode node, List<StandaloneNode> children) {
+    return (node.getNearestAncestor(MsgFallbackGroupNode.class) == null && children.size() == 1 && Iterables.getLast(children) instanceof CallBasicNode)
+        || node.numChildren() == 0;
+  }
+
+  private boolean isValidFirstTag(HtmlOpenTagNode firstTag) {
+    return firstTag.getTagName().isStatic() || firstTag.getTagName().isLegacyDynamicTagName() || !(firstTag.isSelfClosing() || firstTag.getTagName().isDefinitelyVoid());
+  }
+
+  private boolean isSingleTopLevelElement(HtmlOpenTagNode firstTag, HtmlCloseTagNode lastTag) {
+    return lastTag.getTaggedPairs().size() == 1 && Objects.equals(lastTag.getTaggedPairs().get(0), firstTag);
   }
 
   /** Type checks the VE and logonly expressions. */
   private void validateVeLogNode(VeLogNode node) {
     if (node.getVeDataExpression().getRoot().getType().getKind() != Kind.VE_DATA) {
-      reporter.report(
-          node.getVeDataExpression().getSourceLocation(),
-          INVALID_VE,
-          node.getVeDataExpression().getRoot().getType());
+      reporter.report(node.getVeDataExpression().getSourceLocation(), INVALID_VE, node.getVeDataExpression().getRoot().getType());
     }
     if (node.needsSyntheticVelogNode() && isInMsgNode(node)) {
       reporter.report(node.getSourceLocation(), LOG_WITHIN_MESSAGE_REQUIRES_ELEMENT);
     }
+    validateLogonlyExpression(node);
+  }
+
+  private void validateLogonlyExpression(VeLogNode node) {
     if (node.getLogonlyExpression() != null) {
-      // check to see if it is in a msg node.  logonly is disallowed in msg nodes because we don't
-      // have an implementation strategy.
       if (isInMsgNode(node)) {
         reporter.report(node.getLogonlyExpression().getSourceLocation(), LOGONLY_DISALLOWED_IN_MSG);
       }
       SoyType type = node.getLogonlyExpression().getType();
       if (type.getKind() != Kind.BOOL) {
-        reporter.report(
-            node.getLogonlyExpression().getSourceLocation(),
-            WRONG_TYPE,
-            BoolType.getInstance(),
-            type);
+        reporter.report(node.getLogonlyExpression().getSourceLocation(), WRONG_TYPE, BoolType.getInstance(), type);
       }
     }
   }
@@ -286,27 +236,8 @@ final class VeLogValidationPass implements CompilerFileSetPass {
     ExprNode dataExpr = node.getChild(1);
 
     if (veExpr.getType().getKind() == Kind.VE) {
-      if (dataExpr.getType().getKind() != Kind.NULL) {
-        VeType veType = (VeType) veExpr.getType();
-        SoyType dataType = dataExpr.getType();
-        if (!veType.getDataType().isPresent()) {
-          reporter.report(dataExpr.getSourceLocation(), UNEXPECTED_DATA, veType, dataType);
-        } else {
-          SoyType veDataType =
-              typeRegistry.getProtoRegistry().getProtoType(veType.getDataType().get());
-          if (veDataType == null) {
-            reporter.report(veExpr.getSourceLocation(), UNKNOWN_PROTO, veType.getDataType().get());
-          } else if (veDataType.getKind() != Kind.PROTO) {
-            reporter.report(veExpr.getSourceLocation(), BAD_DATA_TYPE, veDataType);
-          } else if (!dataType.equals(veDataType)) {
-            reporter.report(
-                dataExpr.getSourceLocation(), WRONG_TYPE, veType.getDataType().get(), dataType);
-          }
-        }
-      }
+      validateVeData(veExpr, dataExpr);
     } else if (SoyTypes.isKindOrUnionOfKind(veExpr.getType(), Kind.VE)) {
-      // This is a union of VE types with different data types, so it's okay to wrap in ve_data as
-      // long as ve_data's data parameter is null.
       if (dataExpr.getType().getKind() != Kind.NULL) {
         reporter.report(dataExpr.getSourceLocation(), VE_UNION_WITH_DATA, veExpr.getType());
       }
@@ -315,14 +246,34 @@ final class VeLogValidationPass implements CompilerFileSetPass {
     }
   }
 
+  private void validateVeData(ExprNode veExpr, ExprNode dataExpr) {
+    if (dataExpr.getType().getKind() != Kind.NULL) {
+      VeType veType = (VeType) veExpr.getType();
+      SoyType dataType = dataExpr.getType();
+      if (!veType.getDataType().isPresent()) {
+        reporter.report(dataExpr.getSourceLocation(), UNEXPECTED_DATA, veType, dataType);
+      } else {
+        validateProtoType(veExpr, veType, dataExpr, dataType);
+      }
+    }
+  }
+
+  private void validateProtoType(ExprNode veExpr, VeType veType, ExprNode dataExpr, SoyType dataType) {
+    SoyType veDataType = typeRegistry.getProtoRegistry().getProtoType(veType.getDataType().get());
+    if (veDataType == null) {
+      reporter.report(veExpr.getSourceLocation(), UNKNOWN_PROTO, veType.getDataType().get());
+    } else if (veDataType.getKind() != Kind.PROTO) {
+      reporter.report(veExpr.getSourceLocation(), BAD_DATA_TYPE, veDataType);
+    } else if (!dataType.equals(veDataType)) {
+      reporter.report(dataExpr.getSourceLocation(), WRONG_TYPE, veType.getDataType().get(), dataType);
+    }
+  }
+
   private static boolean isInMsgNode(SoyNode node) {
     if (node instanceof MsgNode) {
       return true;
     }
     ParentSoyNode<?> parent = node.getParent();
-    if (parent instanceof MsgBlockNode || parent instanceof MsgSubstUnitNode) {
-      return isInMsgNode(parent);
-    }
-    return false;
+    return (parent instanceof MsgBlockNode || parent instanceof MsgSubstUnitNode) && isInMsgNode(parent);
   }
 }
