4b4540564e4d2344fc676e72f628951d058fdca4
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 13:10:09 -0400

    Your changing file java/src/com/google/template/soy/jbcsrc/shared/Names.java

--- java/src/com/google/template/soy/jbcsrc/shared/Names.java
+++ java/src/com/google/template/soy/jbcsrc/shared/Names.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2015 Google Inc.
  *
@@ -30,9 +31,6 @@ import org.objectweb.asm.Type;
  * files. These utilities are shared between the compiler and the runtime system.
  */
 public final class Names {
-  // Note: This and the plugin path below write into META-INF/services even though they aren't
-  // services, because the surrounding tools already merge data in META-INF/services files.
-  // (The tools do *not* merge other files in META-INF, which is why we hijack the services dir.)
   public static final String META_INF_DELTEMPLATE_PATH =
       "META-INF/services/com.google.template.soy.deltemplates";
 
@@ -64,103 +62,64 @@ public final class Names {
     return isGenerated(type) && SVP_PATTERN.matcher(type.getClassName()).find();
   }
 
-  /**
-   * Translate a user controlled Soy name to a form that is safe to encode in a java class, method
-   * or field name.
-   *
-   * <p>Soy identifiers are very simple, they are restricted to the following regex: {@code
-   * [a-zA-Z_]([a-zA-Z_0-9])*}. So a template name is just one or more identifiers separated by
-   * {@code .} characters. To escape it, we simply replace all '.'s with '_'s, and all '_'s with
-   * '__' and prefix with a package name.
-   */
   public static String javaClassNameFromSoyTemplateName(String soyTemplate) {
-    checkArgument(
-        BaseUtils.isDottedIdentifier(soyTemplate), "%s is not a valid template name.", soyTemplate);
+    checkArgument(BaseUtils.isDottedIdentifier(soyTemplate), "%s is not a valid template name.", soyTemplate);
     int lastDot = soyTemplate.lastIndexOf('.');
     checkArgument(lastDot != -1, "%s should contain a dot", soyTemplate);
     String soyNamespace = soyTemplate.substring(0, lastDot);
+    return getJavaClassName(soyNamespace, soyTemplate);
+  }
+
+  private static String getJavaClassName(String soyNamespace, String soyTemplate) {
     return NamespaceExemptions.isKnownDuplicateNamespace(soyNamespace)
         ? CLASS_PREFIX + soyTemplate
         : CLASS_PREFIX + soyNamespace;
   }
 
-  /**
-   * Translate a user controlled Soy name to a form that is safe to encode in a java class, method
-   * or field name.
-   *
-   * <p>Soy identifiers are very simple, they are restricted to the following regex: {@code
-   * [a-zA-Z_]([a-zA-Z_0-9])*}. So a template name is just one or more identifiers separated by
-   * {@code .} characters. To escape it, we simply replace all '.'s with '_'s, and all '_'s with
-   * '__' and prefix with a package name.
-   */
   public static String javaClassNameFromSoyNamespace(String soyTemplateNamespace) {
-    checkArgument(
-        BaseUtils.isDottedIdentifier(soyTemplateNamespace),
-        "%s is not a valid template namespace.",
-        soyTemplateNamespace);
+    checkArgument(BaseUtils.isDottedIdentifier(soyTemplateNamespace), "%s is not a valid template namespace.", soyTemplateNamespace);
     checkArgument(!NamespaceExemptions.isKnownDuplicateNamespace(soyTemplateNamespace));
     return CLASS_PREFIX + soyTemplateNamespace;
   }
 
-  /** Returns the name for the rendering methods for the given template. */
   public static String renderMethodNameFromSoyTemplateName(String soyTemplate) {
-    checkArgument(
-        BaseUtils.isDottedIdentifier(soyTemplate), "%s is not a valid template name.", soyTemplate);
+    checkArgument(BaseUtils.isDottedIdentifier(soyTemplate), "%s is not a valid template name.", soyTemplate);
     int lastDot = soyTemplate.lastIndexOf('.');
     String soyNamespace = soyTemplate.substring(0, lastDot);
-    if (NamespaceExemptions.isKnownDuplicateNamespace(soyNamespace)) {
-      return "render";
-    }
-    return soyTemplate.substring(lastDot + 1);
+    return NamespaceExemptions.isKnownDuplicateNamespace(soyNamespace) ? "render" : soyTemplate.substring(lastDot + 1);
   }
 
-  /** Returns the name for the {@code ()->CompileTemplate} method for the given template. */
   public static String templateMethodNameFromSoyTemplateName(String soyTemplate) {
-    checkArgument(
-        BaseUtils.isDottedIdentifier(soyTemplate), "%s is not a valid template name.", soyTemplate);
+    checkArgument(BaseUtils.isDottedIdentifier(soyTemplate), "%s is not a valid template name.", soyTemplate);
     int lastDot = soyTemplate.lastIndexOf('.');
     String soyNamespace = soyTemplate.substring(0, lastDot);
-    if (NamespaceExemptions.isKnownDuplicateNamespace(soyNamespace)) {
-      return "template";
-    }
-    return "template$" + soyTemplate.substring(lastDot + 1);
+    return NamespaceExemptions.isKnownDuplicateNamespace(soyNamespace) ? "template" : "template$" + soyTemplate.substring(lastDot + 1);
   }
 
-  /**
-   * Given the soy namespace and file name returns the path where the corresponding resource should
-   * be stored.
-   */
   public static String javaFileName(String soyNamespace, String fileName) {
-    checkArgument(
-        BaseUtils.isDottedIdentifier(soyNamespace),
-        "%s is not a valid soy namspace name.",
-        soyNamespace);
+    checkArgument(BaseUtils.isDottedIdentifier(soyNamespace), "%s is not a valid soy namespace name.", soyNamespace);
 
     String javaClassName = javaClassNameFromSoyTemplateName(soyNamespace + ".foo");
     String javaPackageName = javaClassName.substring(0, javaClassName.lastIndexOf('.'));
-    // convert to a resource path
     return javaPackageName.replace('.', '/') + '/' + fileName;
   }
 
-  /**
-   * Rewrites the given stack trace by replacing all references to generated jbcsrc types with the
-   * original template names.
-   */
   public static void rewriteStackTrace(Throwable throwable) {
     StackTraceElement[] stack = throwable.getStackTrace();
     for (int i = 0; i < stack.length; i++) {
       StackTraceElement curr = stack[i];
       if (isGenerated(curr.getClassName())) {
-        stack[i] =
-            new StackTraceElement(
-                curr.getClassName().substring(CLASS_PREFIX.length()),
-                // Method name == templateName or 'render'. according to
-                // renderMethodNameFromSoyTemplateName
-                curr.getMethodName(),
-                curr.getFileName(),
-                curr.getLineNumber());
+        stack[i] = createNewStackTraceElement(curr);
       }
     }
     throwable.setStackTrace(stack);
+  }
+
+  private static StackTraceElement createNewStackTraceElement(StackTraceElement curr) {
+    return new StackTraceElement(
+        curr.getClassName().substring(CLASS_PREFIX.length()),
+        curr.getMethodName(),
+        curr.getFileName(),
+        curr.getLineNumber());
   }
 }
