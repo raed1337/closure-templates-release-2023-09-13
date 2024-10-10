d1e7d5e476a23e2b5429c1d8356881f5ae280203
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 13:16:32 -0400

    Your changing file java/src/com/google/template/soy/jbcsrc/api/SoySauceBuilder.java

--- java/src/com/google/template/soy/jbcsrc/api/SoySauceBuilder.java
+++ java/src/com/google/template/soy/jbcsrc/api/SoySauceBuilder.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2018 Google Inc.
  *
@@ -48,69 +49,36 @@ public final class SoySauceBuilder {
 
   public SoySauceBuilder() {}
 
-  /**
-   * Sets the plugin instance factories, to be used when constructing the SoySauce.
-   *
-   * <p>These are used to supply the runtime instances needed by SoyJavaSourceFunction
-   * implementations which use the {@code callInstanceMethod} API.
-   *
-   * <p>Note about plugin validation: We considered adding validation in build() to make sure that
-   * withPluginInstances() was called with instance classes for all of the plugins in the meta-inf
-   * plugin files. However, many people use barebones soy sauce builders (e.g. "new
-   * SoySauceBuilder().build()") to render simple templates that won't need plugin instances, and
-   * it's not realistic for all of those cases to pass in plugin instances for all included soy
-   * libraries (esp in shared code). Long term, we could consider adding a "buildWithoutChecks()"
-   * and migrate these cases over, and then we could add validation to build().
-   */
   @CanIgnoreReturnValue
-  public SoySauceBuilder withPluginInstances(
-      Map<String, ? extends Supplier<Object>> pluginInstances) {
+  public SoySauceBuilder withPluginInstances(Map<String, ? extends Supplier<Object>> pluginInstances) {
     this.userPluginInstances = PluginInstances.of(pluginInstances);
     return this;
   }
 
-  /**
-   * Sets the {@link ClassLoader}, to be used when loading generated soy classes.
-   *
-   * <p>In most cases there is no need to use this method, and a default classloader (the one that
-   * loaded this class) will be used. The only use case is when generated templates can not be
-   * located through the standard java binary classpath, so a special classloader can be set to
-   * allow the soy framework to find generated classes.
-   */
   @CanIgnoreReturnValue
   public SoySauceBuilder withClassLoader(ClassLoader loader) {
     this.loader = loader;
     return this;
   }
 
-  /** Sets the user functions. */
   @CanIgnoreReturnValue
-  SoySauceBuilder withFunctions(
-      Iterable<? extends SoyFunction> userFunctions) {
+  SoySauceBuilder withFunctions(Iterable<? extends SoyFunction> userFunctions) {
     this.userFunctions = InternalPlugins.filterDuplicateFunctions(userFunctions);
     return this;
   }
 
-  /**
-   * Sets user directives. Not exposed externally because internal directives should be enough, and
-   * additional functionality can be built as SoySourceFunctions.
-   */
   @CanIgnoreReturnValue
-  SoySauceBuilder withDirectives(
-      Iterable<? extends SoyPrintDirective> userDirectives) {
+  SoySauceBuilder withDirectives(Iterable<? extends SoyPrintDirective> userDirectives) {
     this.userDirectives = InternalPlugins.filterDuplicateDirectives(userDirectives);
     return this;
   }
 
-  /** Non-public; for use by {@link StubbingSoySauce}. */
   @CanIgnoreReturnValue
-  SoySauceBuilder withCustomCompiledTemplatesFactory(
-      CompiledTemplates.Factory compiledTemplatesFactory) {
+  SoySauceBuilder withCustomCompiledTemplatesFactory(CompiledTemplates.Factory compiledTemplatesFactory) {
     this.compiledTemplatesFactory = compiledTemplatesFactory;
     return this;
   }
 
-  /** Creates a SoySauce. */
   public SoySauce build() {
     SoyScopedData scopedData = new SoySimpleScope();
     if (loader == null) {
@@ -119,34 +87,39 @@ public final class SoySauceBuilder {
     return new SoySauceImpl(
         compiledTemplatesFactory.create(readDelTemplatesFromMetaInf(loader), loader),
         scopedData.enterable(),
-        userFunctions, // We don't need internal functions because they only matter at compile time
-        ImmutableList.<SoyPrintDirective>builder()
-            // but internal directives are still required at render time.
-            // in order to handle escaping logging function invocations.
-            .addAll(InternalPlugins.internalDirectives(scopedData))
-            .addAll(userDirectives)
-            .build(),
+        userFunctions,
+        buildUserDirectives(scopedData),
         userPluginInstances);
   }
 
-  /** Walks all resources with the META_INF_DELTEMPLATE_PATH and collects the deltemplates. */
+  private ImmutableList<SoyPrintDirective> buildUserDirectives(SoyScopedData scopedData) {
+    return ImmutableList.<SoyPrintDirective>builder()
+        .addAll(InternalPlugins.internalDirectives(scopedData))
+        .addAll(userDirectives)
+        .build();
+  }
+
   private static ImmutableSet<String> readDelTemplatesFromMetaInf(ClassLoader loader) {
-    URL url = null;
+    ImmutableSet.Builder<String> builder = ImmutableSet.builder();
     try {
-      ImmutableSet.Builder<String> builder = ImmutableSet.builder();
       Enumeration<URL> resources = loader.getResources(Names.META_INF_DELTEMPLATE_PATH);
       while (resources.hasMoreElements()) {
-        url = resources.nextElement();
-        try (InputStream in = url.openStream()) {
-          BufferedReader reader = new BufferedReader(new InputStreamReader(in, UTF_8));
-          for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-            builder.add(line);
-          }
-        }
+        URL url = resources.nextElement();
+        readResourceLines(url, builder);
       }
       return builder.build();
     } catch (IOException iox) {
-      throw new RuntimeException("Unable to read deltemplate listing in " + url, iox);
+      throw new RuntimeException("Unable to read deltemplate listing", iox);
+    }
+  }
+
+  private static void readResourceLines(URL url, ImmutableSet.Builder<String> builder) throws IOException {
+    try (InputStream in = url.openStream();
+         BufferedReader reader = new BufferedReader(new InputStreamReader(in, UTF_8))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        builder.add(line);
+      }
     }
   }
 }
