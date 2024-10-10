61bc9f7336c29bc42a32905ed09557fc8812358c
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 17:52:46 -0400

    Your changing file java/src/com/google/template/soy/internal/util/TreeStreams.java

--- java/src/com/google/template/soy/internal/util/TreeStreams.java
+++ java/src/com/google/template/soy/internal/util/TreeStreams.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2021 Google Inc.
  *
@@ -38,138 +39,98 @@ public final class TreeStreams {
 
   private TreeStreams() {}
 
-  /**
-   * Implements an ancestor search of a tree data structure and returns the result as a Stream.
-   *
-   * @param <T> the type of node in the tree
-   */
   public static <T> Stream<? extends T> ancestor(T root, Function<T, ? extends T> ancestor) {
     return StreamSupport.stream(
         new AbstractSpliterator<>(
-            // Our Baseclass says to pass MAX_VALUE for unsized streams
             Long.MAX_VALUE,
-            // The order is meaningful and every item returned is unique.
             Spliterator.ORDERED | Spliterator.DISTINCT) {
 
-          private T next = root;
+          private T current = root;
 
           @Override
           public boolean tryAdvance(Consumer<? super T> action) {
-            if (next == null) {
+            if (current == null) {
               return false;
             }
-            action.accept(next);
-            next = ancestor.apply(next);
+            action.accept(current);
+            current = ancestor.apply(current);
             return true;
           }
         },
         /* parallel= */ false);
   }
 
-  /**
-   * Implements a breadth-first search of a tree data structure and returns the result as a Stream.
-   *
-   * @param <T> the type of node in the tree
-   */
-  public static <T> Stream<? extends T> breadthFirst(
-      T root, Function<T, Iterable<? extends T>> successors) {
+  public static <T> Stream<? extends T> breadthFirst(T root, Function<T, Iterable<? extends T>> successors) {
     Deque<T> queue = new ArrayDeque<>();
     queue.add(root);
     return StreamSupport.stream(
         new AbstractSpliterator<>(
-            // Our Baseclass says to pass MAX_VALUE for unsized streams
             Long.MAX_VALUE,
-            // The order is meaningful and every item returned is unique.
             Spliterator.ORDERED | Spliterator.DISTINCT) {
           @Override
           public boolean tryAdvance(Consumer<? super T> action) {
-            T next = queue.poll();
-            if (next == null) {
+            T current = queue.poll();
+            if (current == null) {
               return false;
             }
-            Iterables.addAll(queue, successors.apply(next));
-            action.accept(next);
+            Iterables.addAll(queue, successors.apply(current));
+            action.accept(current);
             return true;
           }
         },
         /* parallel= */ false);
   }
 
-  /**
-   * Implements a depth-first search of a tree data structure and returns the result as a Stream.
-   *
-   * @param <T> the type of node in the tree
-   */
-  public static <T> Stream<? extends T> depthFirst(
-      T root, Function<T, Iterable<? extends T>> successors) {
+  public static <T> Stream<? extends T> depthFirst(T root, Function<T, Iterable<? extends T>> successors) {
     Deque<T> stack = new ArrayDeque<>();
     stack.add(root);
     return StreamSupport.stream(
         new AbstractSpliterator<>(
-            // Our Baseclass says to pass MAX_VALUE for unsized streams
             Long.MAX_VALUE,
-            // The order is meaningful and every item returned is unique.
             Spliterator.ORDERED | Spliterator.DISTINCT) {
           @Override
           public boolean tryAdvance(Consumer<? super T> action) {
-            T next = stack.poll();
-            if (next == null) {
+            T current = stack.poll();
+            if (current == null) {
               return false;
             }
 
-            Iterable<? extends T> children = successors.apply(next);
-            List<? extends T> reverseOrder =
-                children instanceof List
-                    ? Lists.reverse((List<? extends T>) children)
-                    : ImmutableList.copyOf(children).reverse();
+            addChildrenToStack(successors.apply(current), stack);
+            action.accept(current);
+            return true;
+          }
+
+          private void addChildrenToStack(Iterable<? extends T> children, Deque<T> stack) {
+            List<? extends T> reverseOrder = children instanceof List
+                ? Lists.reverse((List<? extends T>) children)
+                : ImmutableList.copyOf(children).reverse();
             for (T child : reverseOrder) {
               stack.push(child);
             }
-
-            action.accept(next);
-            return true;
           }
         },
         /* parallel= */ false);
   }
 
-  /**
-   * Creates a lazy stream from the {@code source} stream, optionally collating consecutive items
-   * and merging them into a single item in the resulting stream.
-   *
-   * @param accept predicate that is passed two consecutive items and returns whether they should be
-   *     collated together
-   * @param merger function that merges collated items into a single item
-   */
-  public static <T> Stream<T> collateAndMerge(
-      Stream<? extends T> source, BiPredicate<T, T> accept, Function<List<T>, T> merger) {
-    PeekingIterator<T> i = Iterators.peekingIterator(source.iterator());
+  public static <T> Stream<T> collateAndMerge(Stream<? extends T> source, BiPredicate<T, T> accept, Function<List<T>, T> merger) {
+    PeekingIterator<T> iterator = Iterators.peekingIterator(source.iterator());
     return StreamSupport.stream(
         new AbstractSpliterator<>(
-            // Our Baseclass says to pass MAX_VALUE for unsized streams
             Long.MAX_VALUE,
-            // The order is meaningful.
             Spliterator.ORDERED) {
           @Override
           public boolean tryAdvance(Consumer<? super T> action) {
-            if (!i.hasNext()) {
+            if (!iterator.hasNext()) {
               return false;
             }
-            T next = i.next();
-            List<T> merged = null;
-            while (i.hasNext() && accept.test(next, i.peek())) {
-              if (merged == null) {
-                merged = new ArrayList<>();
-                merged.add(next);
-              }
-              next = i.next();
+            T next = iterator.next();
+            List<T> merged = new ArrayList<>();
+            merged.add(next);
+            while (iterator.hasNext() && accept.test(next, iterator.peek())) {
+              next = iterator.next();
               merged.add(next);
             }
-            if (merged != null) {
-              action.accept(merger.apply(merged));
-            } else {
-              action.accept(next);
-            }
+            action.accept(merged.size() > 1 ? merger.apply(merged) : next);
             return true;
           }
         },
