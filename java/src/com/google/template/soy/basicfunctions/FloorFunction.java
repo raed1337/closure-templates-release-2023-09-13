46f2548f850446eed994dd867d82411989c11c96
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 14:39:23 -0400

    Your changing file java/src/com/google/template/soy/basicfunctions/FloorFunction.java

--- java/src/com/google/template/soy/basicfunctions/FloorFunction.java
+++ java/src/com/google/template/soy/basicfunctions/FloorFunction.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2009 Google Inc.
  *
@@ -32,6 +33,7 @@ import com.google.template.soy.plugin.python.restricted.SoyPythonSourceFunction;
 import com.google.template.soy.shared.restricted.Signature;
 import com.google.template.soy.shared.restricted.SoyFunctionSignature;
 import com.google.template.soy.shared.restricted.SoyPureFunction;
+
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -46,6 +48,9 @@ import java.util.List;
 final class FloorFunction
     implements SoyJavaSourceFunction, SoyJavaScriptSourceFunction, SoyPythonSourceFunction {
 
+  private static final Method FLOOR_FN =
+      JavaValueFactory.createMethod(BasicFunctionsRuntime.class, "floor", SoyValue.class);
+
   @Override
   public JavaScriptValue applyForJavaScriptSource(
       JavaScriptValueFactory factory, List<JavaScriptValue> args, JavaScriptPluginContext context) {
@@ -58,15 +63,9 @@ final class FloorFunction
     return factory.global("int").call(factory.global("math.floor").call(args.get(0)));
   }
 
-  // lazy singleton pattern, allows other backends to avoid the work.
-  private static final class Methods {
-    static final Method FLOOR_FN =
-        JavaValueFactory.createMethod(BasicFunctionsRuntime.class, "floor", SoyValue.class);
-  }
-
   @Override
   public JavaValue applyForJavaSource(
       JavaValueFactory factory, List<JavaValue> args, JavaPluginContext context) {
-    return factory.callStaticMethod(Methods.FLOOR_FN, args.get(0));
+    return factory.callStaticMethod(FLOOR_FN, args.get(0));
   }
 }
