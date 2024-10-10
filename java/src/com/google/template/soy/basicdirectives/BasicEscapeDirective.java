fab23deac797fc0911c9dfa1f193c17e1c233380
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 14:56:13 -0400

    Your changing file java/src/com/google/template/soy/basicdirectives/BasicEscapeDirective.java

--- java/src/com/google/template/soy/basicdirectives/BasicEscapeDirective.java
+++ java/src/com/google/template/soy/basicdirectives/BasicEscapeDirective.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2010 Google Inc.
  *
@@ -116,16 +117,10 @@ public abstract class BasicEscapeDirective
   }
 
   @Override
-  public SoyExpression applyForJbcSrc(
-      JbcSrcPluginContext context, SoyExpression value, List<SoyExpression> args) {
-    // All the escaper functions have versions which accept a raw String and we should prefer
-    // that one if we can avoid boxing.  However, we have to be careful not to throw away
-    // information about the contentKind of the string.  So we only do this if the value is already
-    // unboxed.  In all other cases we call the SoyValue version
-    if (!value.isBoxed()) {
-      return SoyExpression.forString(javaSanitizer(String.class).invoke(value.coerceToString()));
-    }
-    return SoyExpression.forString(javaSanitizer(SoyValue.class).invoke(value));
+  public SoyExpression applyForJbcSrc(JbcSrcPluginContext context, SoyExpression value, List<SoyExpression> args) {
+    return value.isBoxed() 
+        ? SoyExpression.forString(javaSanitizer(SoyValue.class).invoke(value)) 
+        : SoyExpression.forString(javaSanitizer(String.class).invoke(value.coerceToString()));
   }
 
   @VisibleForTesting
@@ -146,22 +141,16 @@ public abstract class BasicEscapeDirective
    * <p>Subclasses can simply add {@code implements Streamable} if they have an implementation in
    * Sanitizers.<name>Streaming. If they don't, this method will throw while trying to find it.
    */
-  public final AppendableAndOptions applyForJbcSrcStreaming(
-      JbcSrcPluginContext context, Expression delegateAppendable, List<SoyExpression> args) {
+  public final AppendableAndOptions applyForJbcSrcStreaming(JbcSrcPluginContext context, Expression delegateAppendable, List<SoyExpression> args) {
     MethodRef sanitizerMethod = javaStreamingSanitizer;
     if (sanitizerMethod == null) {
-      // lazily allocated
-      sanitizerMethod =
-          MethodRef.create(
-              Sanitizers.class, name.substring(1) + "Streaming", LoggingAdvisingAppendable.class);
+      sanitizerMethod = MethodRef.create(Sanitizers.class, name.substring(1) + "Streaming", LoggingAdvisingAppendable.class);
       javaStreamingSanitizer = sanitizerMethod;
     }
     Expression streamingSanitizersExpr = sanitizerMethod.invoke(delegateAppendable);
-    if (isCloseable()) {
-      return AppendableAndOptions.createCloseable(streamingSanitizersExpr);
-    } else {
-      return AppendableAndOptions.create(streamingSanitizersExpr);
-    }
+    return isCloseable() 
+        ? AppendableAndOptions.createCloseable(streamingSanitizersExpr) 
+        : AppendableAndOptions.create(streamingSanitizersExpr);
   }
 
   // -----------------------------------------------------------------------------------------------
@@ -195,10 +184,7 @@ public abstract class BasicEscapeDirective
     }
   }
 
-  /**
-   * Implements the |normalizeHtml directive. This escapes the same as escapeHtml except does not
-   * escape attributes.
-   */
+  /** Implements the |normalizeHtml directive. */
   @SoyPurePrintDirective
   static final class NormalizeHtml extends BasicEscapeDirective implements Streamable {
 
@@ -361,23 +347,17 @@ public abstract class BasicEscapeDirective
     }
 
     @Override
-    public SoyExpression applyForJbcSrc(
-        JbcSrcPluginContext context, SoyExpression value, List<SoyExpression> args) {
-      // escapeJsValue is special since it has special cases for numeric and boolean data
+    public SoyExpression applyForJbcSrc(JbcSrcPluginContext context, SoyExpression value, List<SoyExpression> args) {
       if (!value.isBoxed()) {
         if (value.soyRuntimeType().isKnownString()) {
-          return SoyExpression.forString(
-              javaSanitizer(String.class).invoke(value.coerceToString()));
+          return SoyExpression.forString(javaSanitizer(String.class).invoke(value.coerceToString()));
         }
         if (value.soyRuntimeType().isKnownNumber()) {
-          return SoyExpression.forString(
-              javaSanitizer(double.class).invoke(value.coerceToDouble()));
+          return SoyExpression.forString(javaSanitizer(double.class).invoke(value.coerceToDouble()));
         }
         if (value.soyRuntimeType().isKnownBool()) {
-          return SoyExpression.forString(
-              javaSanitizer(boolean.class).invoke(value.coerceToBoolean()));
+          return SoyExpression.forString(javaSanitizer(boolean.class).invoke(value.coerceToBoolean()));
         }
-        // otherwise fall through to boxing, this handles cases like 'null'
         value = value.box();
       }
       return SoyExpression.forString(javaSanitizer(SoyValue.class).invoke(value));
@@ -466,8 +446,7 @@ public abstract class BasicEscapeDirective
 
   /** Implements the |filterTrustedResourceUri directive. */
   @SoyPurePrintDirective
-  static final class FilterTrustedResourceUri extends BasicEscapeDirective
-      implements ShortCircuitable {
+  static final class FilterTrustedResourceUri extends BasicEscapeDirective implements ShortCircuitable {
 
     FilterTrustedResourceUri() {
       super("|filterTrustedResourceUri");
@@ -484,25 +463,7 @@ public abstract class BasicEscapeDirective
     }
   }
 
-  /**
-   * Implements the |filterHtmlScriptPhrasingData directive.
-   *
-   * <p>Escapes data for embedding in a <script> tag with a non JS content type. (JS content is
-   * handled elsewhere). See
-   * https://html.spec.whatwg.org/multipage/scripting.html#restrictions-for-contents-of-script-elements
-   * for the requirements.
-   *
-   * <p>A streaming implementation is likely feasible but not currently implemented for a few
-   * reasons
-   *
-   * <ul>
-   *   <li>It is unlikely that large amounts of data would be printed in this context.
-   *   <li>It is impossible for logging statements to exist in this context.
-   * </ul>
-   *
-   * So with low motivation and no hard requirement streaming will not be implemented without a
-   * specific well justified request.
-   */
+  /** Implements the |filterHtmlScriptPhrasingData directive. */
   @SoyPurePrintDirective
   static final class FilterHtmlScriptPhrasingData extends BasicEscapeDirective {
 
@@ -516,11 +477,7 @@ public abstract class BasicEscapeDirective
     }
   }
 
-  /**
-   * Implements the |filterCspNonceValue directive.
-   *
-   * <p>See https://www.w3.org/TR/CSP2/#nonce_value
-   */
+  /** Implements the |filterCspNonceValue directive. */
   @SoyPurePrintDirective
   static final class FilterCspNonceValue extends BasicEscapeDirective {
 
@@ -536,8 +493,7 @@ public abstract class BasicEscapeDirective
 
   /** Implements the |whitespaceHtmlAttributes directive. */
   @SoyPurePrintDirective
-  public static final class WhitespaceHtmlAttributesDirective extends BasicEscapeDirective
-      implements Streamable {
+  public static final class WhitespaceHtmlAttributesDirective extends BasicEscapeDirective implements Streamable {
 
     public WhitespaceHtmlAttributesDirective() {
       super("|whitespaceHtmlAttributes");
