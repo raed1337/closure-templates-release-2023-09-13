de0d8f3df0050ee87e5e655647204837fc31758f
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 15:48:55 -0400

    Your changing file java/src/com/google/template/soy/passes/CheckValidVarrefsPass.java

--- java/src/com/google/template/soy/passes/CheckValidVarrefsPass.java
+++ java/src/com/google/template/soy/passes/CheckValidVarrefsPass.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2021 Google Inc.
  *
@@ -53,6 +54,13 @@ final class CheckValidVarrefsPass implements CompilerFilePass {
   }
 
   private void checkVarRef(VarRefNode varRef) {
+    validateBadSoyTypes(varRef);
+    validateProtoType(varRef);
+    validateProtoExtension(varRef);
+    validateCssType(varRef);
+  }
+
+  private void validateBadSoyTypes(VarRefNode varRef) {
     VarDefn defn = varRef.getDefnDecl();
     if (defn != null && defn.hasType()) {
       if (BAD_SOY_TYPE_KINDS.contains(defn.type().getKind())) {
@@ -60,38 +68,29 @@ final class CheckValidVarrefsPass implements CompilerFilePass {
             varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
       }
     }
+  }
 
+  private void validateProtoType(VarRefNode varRef) {
     if (varRef.getType().getKind() == SoyType.Kind.PROTO_TYPE
         && !(varRef.getParent() == null
             || varRef.getParent().getKind() == ExprNode.Kind.FIELD_ACCESS_NODE
             || varRef.getParent().getKind() == ExprNode.Kind.FUNCTION_NODE)) {
-      // This is for imports like:
-      // import {MyMessage} from 'path/to/my/proto/file.proto';
-      // These are only allowed in:
-      // - Proto init calls (i.e. `MyMessage()`). In this case, the parent is null (the VarRefNode
-      //   is the `nameExpr` on the FunctionNode).
-      // - Field accesses, like `MyMessage.MySubMessage`
-      // - `ve_def` data proto type parameters (i.e. `ve_def('MyVe', 123, MyMessage)`). However,
-      //   this allows any FunctionNode, not just `ve_def`, to prevent duplicate/confusing errors on
-      //   things like misspelling `ve_def`. Function parameters are type checked elsewhere, which
-      //   which will report any issues.
       errorReporter.report(
           varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
-    } else if (varRef.getType().getKind() == SoyType.Kind.PROTO_EXTENSION
+    }
+  }
+
+  private void validateProtoExtension(VarRefNode varRef) {
+    if (varRef.getType().getKind() == SoyType.Kind.PROTO_EXTENSION
         && varRef.getParent().getKind() != ExprNode.Kind.METHOD_CALL_NODE) {
-      // This is for imports like:
-      // import {TopLevelEnum} from 'path/to/my/proto/file.proto';
-      // These are only allowed as the parameter to the `getExtension`, `getReadonlyExtension` and
-      // `hasExtension` proto methods. However, this allows any MethodCallNode to prevent
-      // duplicate/confusing errors on things like misspelling one of the method names. Method
-      // parameters are type checked elsewhere, which which will report any issues.
       errorReporter.report(
           varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
-    } else if (varRef.getType().getKind() == SoyType.Kind.CSS_TYPE
+    }
+  }
+
+  private void validateCssType(VarRefNode varRef) {
+    if (varRef.getType().getKind() == SoyType.Kind.CSS_TYPE
         && varRef.getParent().getKind() != ExprNode.Kind.FIELD_ACCESS_NODE) {
-      // This is for imports like:
-      // import {classes} from 'path/to/my/css/file/css';
-      // These are only allowed in field accesses, like `classes.myStyleClassName`
       errorReporter.report(
           varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
     }
