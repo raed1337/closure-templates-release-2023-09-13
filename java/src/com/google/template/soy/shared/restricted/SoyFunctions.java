24fbf033806f74a83abd0d746aec6fdae502ee78
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 19:58:34 -0400

    Your changing file java/src/com/google/template/soy/shared/restricted/SoyFunctions.java

--- java/src/com/google/template/soy/shared/restricted/SoyFunctions.java
+++ java/src/com/google/template/soy/shared/restricted/SoyFunctions.java
@@ -1,17 +1,19 @@
+
 /*
  * Copyright 2020 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0
+ *
+ * (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 package com.google.template.soy.shared.restricted;
@@ -34,11 +36,19 @@ public final class SoyFunctions {
    */
   public static boolean isPure(Object function) {
     if (function instanceof SoyFunction) {
-      return ((SoyFunction) function).isPure();
+      return isSoyFunctionPure((SoyFunction) function);
     } else if (function instanceof SoySourceFunction) {
-      return function.getClass().isAnnotationPresent(SoyPureFunction.class);
+      return isSoySourceFunctionPure((SoySourceFunction) function);
     } else {
       throw new ClassCastException(function.getClass().getName());
     }
+  }
+
+  private static boolean isSoyFunctionPure(SoyFunction function) {
+    return function.isPure();
+  }
+
+  private static boolean isSoySourceFunctionPure(SoySourceFunction function) {
+    return function.getClass().isAnnotationPresent(SoyPureFunction.class);
   }
 }
