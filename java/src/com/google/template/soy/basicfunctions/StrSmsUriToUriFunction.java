3a0a6b2f13e48a58d8afa558d534a2bf64c30081
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 14:47:16 -0400

    Your changing file java/src/com/google/template/soy/basicfunctions/StrSmsUriToUriFunction.java

--- java/src/com/google/template/soy/basicfunctions/StrSmsUriToUriFunction.java
+++ java/src/com/google/template/soy/basicfunctions/StrSmsUriToUriFunction.java
@@ -1,15 +1,17 @@
+
 /*
  * Copyright 2018 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0
+ *
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -46,6 +48,9 @@ import java.util.List;
 final class StrSmsUriToUriFunction
     implements SoyJavaSourceFunction, SoyJavaScriptSourceFunction, SoyPythonSourceFunction {
 
+  private static final Method SMS_TO_URI =
+      JavaValueFactory.createMethod(Sanitizers.class, "filterSmsUri", String.class);
+
   @Override
   public JavaScriptValue applyForJavaScriptSource(
       JavaScriptValueFactory factory, List<JavaScriptValue> args, JavaScriptPluginContext context) {
@@ -58,15 +63,9 @@ final class StrSmsUriToUriFunction
     return factory.global("sanitize.sms_to_uri").call(args.get(0).coerceToString());
   }
 
-  // Lazy singleton pattern, allows other backends to avoid the work.
-  private static final class Methods {
-    static final Method SMS_TO_URI =
-        JavaValueFactory.createMethod(Sanitizers.class, "filterSmsUri", String.class);
-  }
-
   @Override
   public JavaValue applyForJavaSource(
       JavaValueFactory factory, List<JavaValue> args, JavaPluginContext context) {
-    return factory.callStaticMethod(Methods.SMS_TO_URI, args.get(0));
+    return factory.callStaticMethod(SMS_TO_URI, args.get(0));
   }
 }
