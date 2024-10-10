7de3f6f8b3dc4a7ddd9057cf55b6b1764728ce18
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 11:50:15 -0400

    Your changing file java/src/com/google/template/soy/jbcsrc/SimpleLocalVariableManager.java

--- java/src/com/google/template/soy/jbcsrc/SimpleLocalVariableManager.java
+++ java/src/com/google/template/soy/jbcsrc/SimpleLocalVariableManager.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2019 Google Inc.
  *
@@ -45,7 +46,6 @@ import org.objectweb.asm.Type;
  * <p>In bytecode local variables are assigned indices which we need to manage.
  */
 final class SimpleLocalVariableManager implements LocalVariableManager {
-  // Locals have the same rules as field names so we can just use the same mangling strategy.
   private final ArrayDeque<UniqueNameGenerator> localNames = new ArrayDeque<>();
   private final List<LocalVariable> allVariables = new ArrayList<>();
   private final BitSet availableSlots = new BitSet();
@@ -81,28 +81,35 @@ final class SimpleLocalVariableManager implements LocalVariableManager {
     if (!isStatic) {
       reserveParameter("this", ownerType);
     }
-    int parameterIndex = 0;
-    for (Type type : argumentTypes) {
-      reserveParameter(parameterNames.get(parameterIndex), type);
-      parameterIndex++;
+    for (int parameterIndex = 0; parameterIndex < argumentTypes.length; parameterIndex++) {
+      reserveParameter(parameterNames.get(parameterIndex), argumentTypes[parameterIndex]);
     }
   }
 
   public void updateParameterTypes(Type[] parameterTypes, List<String> parameterNames) {
-    Set<String> allAllocatedVariableNames =
-        allVariables.stream().map(v -> v.variableName()).collect(toCollection(HashSet::new));
-    // We could support more changes (e.g. appending parameters, support instance methods), but
-    // it doesn't appear to be needed and this is simpler.
+    validateParameterUpdate();
+    this.parameterTypes = parameterTypes;
+    int spaceForParameters = calculateSpaceForParameters(parameterTypes);
+    shiftAllocatedVariables(spaceForParameters);
+    allocateNewParameters(parameterTypes, parameterNames);
+  }
+
+  private void validateParameterUpdate() {
     checkArgument(
         this.parameterTypes.length == 0 && isStatic,
         "Can only change parameters if the variable manager original had no parameters and was"
             + " static.");
-    this.parameterTypes = parameterTypes;
+  }
+
+  private int calculateSpaceForParameters(Type[] parameterTypes) {
     int spaceForParameters = 0;
     for (Type type : parameterTypes) {
       spaceForParameters += type.getSize();
     }
-    // We need to shift all allocated variables to account for the new parameters.
+    return spaceForParameters;
+  }
+
+  private void shiftAllocatedVariables(int spaceForParameters) {
     for (int i = availableSlots.size() - 1; (i = availableSlots.previousSetBit(i)) >= 0; ) {
       availableSlots.set(i + spaceForParameters);
       availableSlots.clear(i);
@@ -110,17 +117,23 @@ final class SimpleLocalVariableManager implements LocalVariableManager {
     for (var var : allVariables) {
       var.shiftIndex(spaceForParameters);
     }
-    // now allocate the parameters
-    int parameterIndex = 0;
-    for (Type type : parameterTypes) {
+  }
+
+  private void allocateNewParameters(Type[] parameterTypes, List<String> parameterNames) {
+    Set<String> allAllocatedVariableNames =
+        allVariables.stream().map(LocalVariable::variableName).collect(toCollection(HashSet::new));
+    for (int parameterIndex = 0; parameterIndex < parameterTypes.length; parameterIndex++) {
       String name = parameterNames.get(parameterIndex);
-      // because we are adding names late we need to mangle
-      while (!allAllocatedVariableNames.add(name)) {
-        name = "$" + name;
-      }
-      reserveParameter(name, type);
-      parameterIndex++;
+      name = mangleNameIfNeeded(allAllocatedVariableNames, name);
+      reserveParameter(name, parameterTypes[parameterIndex]);
     }
+  }
+
+  private String mangleNameIfNeeded(Set<String> allAllocatedVariableNames, String name) {
+    while (!allAllocatedVariableNames.add(name)) {
+      name = "$" + name;
+    }
+    return name;
   }
 
   private void reserveParameter(String name, Type type) {
@@ -153,11 +166,15 @@ final class SimpleLocalVariableManager implements LocalVariableManager {
   public void generateTableEntries(CodeBuilder cb) {
     generated = true;
     for (LocalVariable var : allVariables) {
-      try {
-        var.tableEntry(cb);
-      } catch (Throwable t) {
-        throw new RuntimeException("unable to write table entry for: " + var, t);
-      }
+      generateTableEntry(cb, var);
+    }
+  }
+
+  private void generateTableEntry(CodeBuilder cb, LocalVariable var) {
+    try {
+      var.tableEntry(cb);
+    } catch (Throwable t) {
+      throw new RuntimeException("unable to write table entry for: " + var, t);
     }
   }
 
@@ -215,15 +232,12 @@ final class SimpleLocalVariableManager implements LocalVariableManager {
     int size = type.getSize();
     checkArgument(size == 1 || size == 2); // void has size 0
     int start = 0;
-    // https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.7.3
-    // this is the maximum number of local variables
     while (start < 65536) {
       int nextClear = availableSlots.nextClearBit(start);
       if (size == 1 || (size == 2 && !availableSlots.get(nextClear + 1))) {
         availableSlots.set(nextClear, nextClear + size);
         return nextClear;
       }
-      // keep looking
       start = nextClear + 1;
     }
     throw new RuntimeException("too many local variables");
