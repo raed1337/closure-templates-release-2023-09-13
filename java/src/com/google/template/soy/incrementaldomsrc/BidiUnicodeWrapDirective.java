ac2d7cd078cdbc9ffe174ab4b7c89ad63d86e172
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 11:15:14 -0400

    Your changing file java/src/com/google/template/soy/incrementaldomsrc/BidiUnicodeWrapDirective.java

--- java/src/com/google/template/soy/incrementaldomsrc/BidiUnicodeWrapDirective.java
+++ java/src/com/google/template/soy/incrementaldomsrc/BidiUnicodeWrapDirective.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2019 Google Inc.
  *
@@ -58,14 +59,17 @@ final class BidiUnicodeWrapDirective implements SoyLibraryAssistedJsSrcPrintDire
 
   @Override
   public JsExpr applyForJsSrc(JsExpr value, List<JsExpr> args) {
-    return new JsExpr(
-        String.format(
-            "goog.module.get('%s').$$bidiUnicodeWrap(%s, %s, %s)",
-            "google3.javascript.template.soy.soyutils_directives",
-            bidiGlobalDir.getCodeSnippet(),
-            value.getText(),
-            INCREMENTAL_DOM_PARAM_NAME),
-        Integer.MAX_VALUE);
+    return createJsExpr(value);
+  }
+
+  private JsExpr createJsExpr(JsExpr value) {
+    String jsCode = String.format(
+        "goog.module.get('%s').$$bidiUnicodeWrap(%s, %s, %s)",
+        "google3.javascript.template.soy.soyutils_directives",
+        bidiGlobalDir.getCodeSnippet(),
+        value.getText(),
+        INCREMENTAL_DOM_PARAM_NAME);
+    return new JsExpr(jsCode, Integer.MAX_VALUE);
   }
 
   @Override
