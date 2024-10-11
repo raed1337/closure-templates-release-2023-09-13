185a67ac67516375256327b897e8332ada26c8ab
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 21:12:46 -0400

    Your changing file java/src/com/google/template/soy/pysrc/restricted/PyFunctionExprBuilder.java

--- java/src/com/google/template/soy/pysrc/restricted/PyFunctionExprBuilder.java
+++ java/src/com/google/template/soy/pysrc/restricted/PyFunctionExprBuilder.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2015 Google Inc.
  *
@@ -55,32 +56,27 @@ public final class PyFunctionExprBuilder {
 
   @CanIgnoreReturnValue
   public PyFunctionExprBuilder addArg(String str) {
-    this.argList.add(new PyStringExpr("'" + str + "'"));
-    return this;
+    return addArg(new PyStringExpr("'" + str + "'"));
   }
 
   @CanIgnoreReturnValue
   public PyFunctionExprBuilder addArg(boolean b) {
-    this.argList.add(new PyExpr(b ? "True" : "False", Integer.MAX_VALUE));
-    return this;
+    return addArg(new PyExpr(b ? "True" : "False", Integer.MAX_VALUE));
   }
 
   @CanIgnoreReturnValue
   public PyFunctionExprBuilder addArg(int i) {
-    this.argList.add(new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
-    return this;
+    return addArg(new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
   }
 
   @CanIgnoreReturnValue
   public PyFunctionExprBuilder addArg(double i) {
-    this.argList.add(new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
-    return this;
+    return addArg(new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
   }
 
   @CanIgnoreReturnValue
   public PyFunctionExprBuilder addArg(long i) {
-    this.argList.add(new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
-    return this;
+    return addArg(new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
   }
 
   @CanIgnoreReturnValue
@@ -101,26 +97,22 @@ public final class PyFunctionExprBuilder {
 
   @CanIgnoreReturnValue
   public PyFunctionExprBuilder addKwarg(String key, String str) {
-    kwargMap.put(key, new PyStringExpr("'" + str + "'"));
-    return this;
+    return addKwarg(key, new PyStringExpr("'" + str + "'"));
   }
 
   @CanIgnoreReturnValue
   public PyFunctionExprBuilder addKwarg(String key, int i) {
-    kwargMap.put(key, new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
-    return this;
+    return addKwarg(key, new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
   }
 
   @CanIgnoreReturnValue
   public PyFunctionExprBuilder addKwarg(String key, double i) {
-    kwargMap.put(key, new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
-    return this;
+    return addKwarg(key, new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
   }
 
   @CanIgnoreReturnValue
   public PyFunctionExprBuilder addKwarg(String key, long i) {
-    kwargMap.put(key, new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
-    return this;
+    return addKwarg(key, new PyExpr(String.valueOf(i), Integer.MAX_VALUE));
   }
 
   /**
@@ -137,41 +129,44 @@ public final class PyFunctionExprBuilder {
     if (unpackedKwargs != null) {
       throw new UnsupportedOperationException("Only one kwarg unpacking allowed per expression.");
     }
+    unpackedKwargs = generateUnpackedKwargs(mapping);
+    return this;
+  }
+
+  private String generateUnpackedKwargs(PyExpr mapping) {
     StringBuilder expr = new StringBuilder("**");
     if (mapping.getPrecedence() < Integer.MAX_VALUE) {
       expr.append("(").append(mapping.getText()).append(")");
     } else {
       expr.append(mapping.getText());
     }
-    unpackedKwargs = expr.toString();
-    return this;
+    return expr.toString();
   }
 
   /** Returns a valid Python function call as a String. */
   public String build() {
     StringBuilder sb = new StringBuilder(funcName + "(");
 
-    // Join args and kwargs into simple strings.
-    String args =
-        argList.stream()
+    String args = formatArgs();
+    String kwargs = formatKwargs();
+
+    Joiner.on(", ").skipNulls().appendTo(sb, args, kwargs, unpackedKwargs);
+    sb.append(")");
+    return sb.toString();
+  }
+
+  private String formatArgs() {
+    return argList.stream()
             .map(PyExpr::getText)
             .filter(Objects::nonNull)
             .collect(Collectors.joining(", "));
-    String kwargs =
-        kwargMap.entrySet().stream()
+  }
+
+  private String formatKwargs() {
+    return kwargMap.entrySet().stream()
             .map(entry -> entry.getKey() + "=" + entry.getValue().getText())
             .filter(Objects::nonNull)
             .collect(Collectors.joining(", "));
-
-    // Strip empty strings.
-    args = Strings.emptyToNull(args);
-    kwargs = Strings.emptyToNull(kwargs);
-
-    // Join all pieces together.
-    Joiner.on(", ").skipNulls().appendTo(sb, args, kwargs, unpackedKwargs);
-
-    sb.append(")");
-    return sb.toString();
   }
 
   /**
