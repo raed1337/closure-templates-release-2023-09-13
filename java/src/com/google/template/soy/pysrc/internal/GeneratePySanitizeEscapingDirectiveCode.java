23554cb688752a2a12c181fb81233226cb7a3225
mohamedelaoun <mohamedelaoun@Mohameds-Air>
2024-10-10 21:23:53 -0400

    Your changing file java/src/com/google/template/soy/pysrc/internal/GeneratePySanitizeEscapingDirectiveCode.java

--- java/src/com/google/template/soy/pysrc/internal/GeneratePySanitizeEscapingDirectiveCode.java
+++ java/src/com/google/template/soy/pysrc/internal/GeneratePySanitizeEscapingDirectiveCode.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2014 Google Inc.
  *
@@ -76,9 +77,10 @@ public final class GeneratePySanitizeEscapingDirectiveCode
 
   @Override
   protected String escapeOutputString(String input) {
-    String escapeCharacters = "\\'\"\b\f\n\r\t";
+    return escapeString(input, "\\'\"\b\f\n\r\t");
+  }
 
-    // Give the string builder a little bit of extra space to account for new escape characters.
+  private String escapeString(String input, String escapeCharacters) {
     StringBuilder result = new StringBuilder((int) (input.length() * 1.2));
     for (char c : input.toCharArray()) {
       if (escapeCharacters.indexOf(c) != -1) {
@@ -86,27 +88,31 @@ public final class GeneratePySanitizeEscapingDirectiveCode
       }
       result.append(c);
     }
-
     return result.toString();
   }
 
   @Override
   protected String convertFromJavaRegex(Pattern javaPattern) {
-    String body =
-        javaPattern
-            .pattern()
-            .replace("\r", "\\r")
-            .replace("\n", "\\n")
-            .replace("\u2028", "\\u2028")
-            .replace("\u2029", "\\u2029")
-            .replace("\\z", "\\Z");
-    // DOTALL is not allowed to keep the syntax simple (it's also not available in JavaScript).
+    String body = convertPatternBody(javaPattern);
     if ((javaPattern.flags() & Pattern.DOTALL) != 0) {
       throw new IllegalArgumentException("Pattern " + javaPattern + " uses DOTALL.");
     }
 
+    return buildRegexPattern(javaPattern, body);
+  }
+
+  private String convertPatternBody(Pattern javaPattern) {
+    return javaPattern
+        .pattern()
+        .replace("\r", "\\r")
+        .replace("\n", "\\n")
+        .replace("\u2028", "\\u2028")
+        .replace("\u2029", "\\u2029")
+        .replace("\\z", "\\Z");
+  }
+
+  private String buildRegexPattern(Pattern javaPattern, String body) {
     StringBuilder buffer = new StringBuilder(body.length() + 40);
-    // Default to using unicode character classes.
     buffer.append("re.compile(r\"\"\"").append(body).append("\"\"\", re.U");
     if ((javaPattern.flags() & Pattern.CASE_INSENSITIVE) != 0) {
       buffer.append(" | re.I");
@@ -120,7 +126,6 @@ public final class GeneratePySanitizeEscapingDirectiveCode
 
   @Override
   protected void generatePrefix(StringBuilder outputCode) {
-    // Emulate Python 3 style unicode string literals, and import necessary libraries.
     outputCode
         .append("# pylint:skip-file\n")
         .append("from __future__ import unicode_literals\n")
@@ -132,8 +137,6 @@ public final class GeneratePySanitizeEscapingDirectiveCode
         .append("except ImportError:\n")
         .append("  from urllib import quote  # Python 2\n")
         .append("\n")
-        .append("# An empty string which is 'str' type in Python 2 (i.e. a bytestring)\n")
-        .append("# and 'str' type in Python 3 (i.e. a unicode string).\n")
         .append("ACTUALLY_STR_EMPTY_STRING = str()\n")
         .append("\n")
         .append("try:\n")
@@ -175,10 +178,6 @@ public final class GeneratePySanitizeEscapingDirectiveCode
   protected void useExistingLibraryFunction(
       StringBuilder outputCode, String identifier, String existingFunction) {
     String fnName = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, identifier);
-    // When quote is called without a second parameter, '/' is by default NOT escaped.
-    // Adding an empty string as a second parameter ensures that '/' is escaped.
-    // Documentation: https://docs.python.org/3/library/urllib.parse.html#urllib.parse.quote
-    //                https://docs.python.org/2/library/urllib.html#urllib.quote
     outputCode
         .append("\ndef ")
         .append(fnName)
@@ -198,21 +197,24 @@ public final class GeneratePySanitizeEscapingDirectiveCode
         .append(fnName)
         .append("_helper(value):\n")
         .append("  value = str(value)\n");
+    addFilterCheck(outputCode, digest);
+    handleEscapes(outputCode, digest);
+    outputCode.append("\n");
+  }
+
+  private void addFilterCheck(StringBuilder outputCode, DirectiveDigest digest) {
     if (digest.getFilterName() != null) {
       String filterName = digest.getFilterName();
       outputCode.append("  if not _FILTER_FOR_").append(filterName).append(".search(value):\n");
-      // TODO(dcphillips): Raising a debugging assertion error could be useful.
       outputCode
           .append("    return '")
           .append(digest.getInnocuousOutput())
           .append("'\n")
           .append("\n");
     }
+  }
 
-    if (digest.getNonAsciiPrefix() != null) {
-      // TODO: We can add a second replace of all non-ascii codepoints below.
-      throw new UnsupportedOperationException("Non ASCII prefix escapers not implemented yet.");
-    }
+  private void handleEscapes(StringBuilder outputCode, DirectiveDigest digest) {
     if (digest.getEscapesName() != null) {
       String escapeMapName =
           CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_UNDERSCORE, digest.getEscapesName());
@@ -227,13 +229,10 @@ public final class GeneratePySanitizeEscapingDirectiveCode
     } else {
       outputCode.append("  return value\n");
     }
-    outputCode.append("\n");
   }
 
   @Override
   protected void generateCommonConstants(StringBuilder outputCode) {
-    // Emit patterns and constants needed by escaping functions that are not part of any one
-    // escaping convention.
     outputCode
         .append("_HTML_TAG_REGEX = ")
         .append(convertFromJavaRegex(EscapingConventions.HTML_TAG_CONTENT))
